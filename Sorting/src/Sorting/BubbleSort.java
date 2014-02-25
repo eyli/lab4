92e5159b4b4ac2b19df87235785d865783526ece
Evan Li <evan_li@brown.edu>
2014-02-24 20:11:52 -0500

    Implemented BubbleSort.java

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -7,8 +7,24 @@ public class BubbleSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
+		int j;
+		 boolean flag = true;   // set flag to true to begin first pass
+		 int temp;   //holding variable
+
+	     while ( flag )
+	     {
+			flag= false;    //set flag to false awaiting a possible swap
+			for( j=0;  j < unsortedArray.length -1;  j++ )
+			{
+			       if ( unsortedArray[ j ] < unsortedArray[j+1] )   // change to > for ascending sort
+			   {
+			           temp = unsortedArray[ j ];                //swap elements
+			   unsortedArray[ j ] = unsortedArray[ j+1 ];
+			   unsortedArray[ j+1 ] = temp;
+			  flag = true;              //shows a swap occurred  
+			              } 
+			        } 
+			  }
+	     return unsortedArray;
 	}
 }
